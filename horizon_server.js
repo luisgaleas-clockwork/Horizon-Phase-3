f1f77d5a791f7d0ac20af2e2392a05c6ac365a84
Luis Galeas <luis.galeasjr@gmail.com>
2019-09-27 18:16:54 -0400

    updated main server and packagees

--- horizon_server.js
+++ horizon_server.js
@@ -1,18 +1,26 @@
 const express = require ("express");
 const app = express();
-const cors = require ("cors");
+const helmet = require ("helmet")
+
 const port = 4000
 
 const productsAPI = require("./routes/productsAPI");
 const contactsAPI = require("./routes/contactAPI");
-const product_idAPI = require("./routes/product_idAPI")
+const productsFilterAPI = require("./routes/porductsFilterAPI")
 
-app.use(cors("*"))
+
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
 
 app.use("/api", productsAPI)
 app.use("/api", contactsAPI)
-app.use("/api", product_idAPI)
+app.use("/api", productsFilterAPI)
 
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirnamem,"./client/build/index.html"))
+})
 
 app.listen(port, () =>{
     console.log(`products server listening on port ${port}`)
