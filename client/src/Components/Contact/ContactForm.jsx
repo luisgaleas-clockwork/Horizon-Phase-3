8251233d209d0c67ce95737f01dd77e8602182d9
Luis Galeas <luis.galeasjr@gmail.com>
2019-10-02 17:23:02 -0400

    added comments to contact page and added new fonts to page.

--- client/src/Components/Contact/ContactForm.jsx
+++ client/src/Components/Contact/ContactForm.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 
-const Form = () => {
+const Form = (props) => {
+    const {getInfo, submit} =props
     return (
         <main className="form-main">
             <section className="form-bg">
-                <form className="needs-validation form-container" noValidate>
-                <h1>Contact Us</h1>
-                    <input type="text"  id="validationCustom012" placeholder="Name"
-                        required />
-                    <input type="email"  id="validationCustom022" placeholder="Email" required />
-                    <textarea></textarea>
-                    <button type="submit">Submit</button>
+                <form className="needs-validation form-container" method="POST">
+                <h1>Give us feedback</h1>
+                    <input type="text"  name="first_name" placeholder="First name" onChange={getInfo} required />
+                    <input type="text"  name="last_name" placeholder="Last name" onChange={getInfo} required />
+                    <input type="email" name="email"  placeholder="Email" id="validationCustom022" placeholder="Email" onChange={getInfo} required />
+                    <textarea name="contact_comment" placeholder="Give us Feedback" onChange={getInfo}></textarea>
+                    <button type="submit" onClick={submit}>Submit</button>
                 </form>
             </section>
         </main>
--- client/src/SitePages/ContactPage.js
+++ client/src/SitePages/ContactPage.js
@@ -6,8 +6,32 @@ class Contact extends React.Component {
     constructor() {
         super()
         this.state = {
-            contact: []
+            contact: [],
+            first_name: "",
+            last_name: "",
+            email: "",
+            contact_comment: ""
         }
+    }
+
+    getInfo = (e) => {
+        this.setState({ [e.target.name]: e.target.value })
+        console.log(this.state)
+    }
+    submitInfo = (e) => {
+        e.preventDefault()
+        fetch('api/comment', {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                first_name: this.state.first_name,
+                last_name: this.state.last_name,
+                email: this.state.email,
+                contact_comment: this.state.contact_comment
+            })
+        })
     }
 
     componentDidMount() {
@@ -30,7 +54,7 @@ class Contact extends React.Component {
         console.log(`this.state -`, this.state)
         return (
             <div className="contact-main">
-                <Form />
+                <Form getInfo={this.getInfo} submit={this.submitInfo} />
                 <h2>Customer Feedback</h2>
                 <div className="comment_section">
                     {this.state.contact.map((info) => {
